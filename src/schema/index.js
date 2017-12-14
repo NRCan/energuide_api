401d8015cd555e1483cc7b2c95b0b9aa4d4e6fb6
evadb <evademersbrett@gmail.com>
2017-12-14 12:17:20 -0500

    Add i18n to graphql descriptions

---
+++ src/schema/index.js
@@ -0,0 +1,36 @@
+import resolvers from './resolvers'
+import { makeExecutableSchema } from 'graphql-tools'
+
+const Schema = i18n => {
+
+  const typeDefs =`
+    type Query {
+      evaluations: [Evaluation]
+    }
+    
+    # ${i18n.t`This is a description of evaluations`}
+    type Evaluation {
+      # ${i18n.t`Year house was built in`}
+      yearBuilt: String
+      # ${i18n.t`Square footage of home`}
+      floorArea: String
+      # ${i18n.t`Footprint`}
+      footprint: String
+      # ${i18n.t`Type of furnace in home`}
+      furnaceType: String
+      # ${i18n.t`Type of fuel that furnace runs on`}
+      furnaceFuel: String
+    }
+  `
+
+  return makeExecutableSchema({typeDefs, resolvers})
+
+}
+
+export default Schema
+
+
+
+
+
+
--- src/server.js
+++ src/server.js
@@ -2,20 +2,33 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 import { makeExecutableSchema } from 'graphql-tools'
-import typeDefs from './typedefs'
-import resolvers from './resolvers'
+import Schema from './schema'
+import { i18n , unpackCatalog } from 'lingui-i18n'
+import requestLanguage from 'express-request-language'
 
-const schema = makeExecutableSchema({ typeDefs, resolvers })
+i18n.load({ 
+  fr: unpackCatalog(require('./locale/fr/messages.js')),
+  en: unpackCatalog(require('./locale/en/messages.js')),
+})
 
 function Server(context = {}) {
   const server = express()
-  server.use(
+  server
+  .use(
+    requestLanguage({
+      languages: i18n.availableLanguages.sort(),
+    }),
+  )
+  .use(
     '/graphql',
     bodyParser.json(),
-    graphqlExpress(request => ({
-      schema,
-      context,
-    })),
+    graphqlExpress(request => {
+      i18n.activate(request.language)
+      return {
+        schema: new Schema(i18n),
+        context,
+      }
+    }),
   )
   server.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
   return server
