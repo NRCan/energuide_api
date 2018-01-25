87bc9bf212ef6c2eaead74797f9f2726fbe6607e
Jason White <jason.white@cds-snc.ca>
2018-01-25 16:10:39 -0500

    add cli load command

--- python/src/energuide/cli.py
+++ python/src/energuide/cli.py
@@ -1,2 +1,29 @@
+import click
+from energuide import database
+from energuide import transform
+
+
+@click.group()
 def main() -> None:
-    print("Hello")
+    pass
+
+
+@main.command()
+@click.option('--username', envvar=database.EnvVariables.username.value, default=database.EnvDefaults.username.value)
+@click.option('--password', envvar=database.EnvVariables.password.value, default=database.EnvDefaults.password.value)
+@click.option('--host', envvar=database.EnvVariables.host.value, default=database.EnvDefaults.host.value)
+@click.option('--port', envvar=database.EnvVariables.port.value, default=database.EnvDefaults.port.value, type=int)
+@click.option('--db_name', envvar=database.EnvVariables.database.value, default=database.EnvDefaults.database.value)
+@click.option('--collection',
+              envvar=database.EnvVariables.collection.value,
+              default=database.EnvDefaults.collection.value)
+@click.option('--filename', type=click.Path(exists=True), required=True)
+def load(username: str, password: str, host: str, port: int, db_name: str, collection: str, filename: str) -> None:
+    coords = database.DatabaseCoordinates(
+        username=username,
+        password=password,
+        host=host,
+        port=port
+    )
+
+    transform.run(coords, db_name, collection, filename)
