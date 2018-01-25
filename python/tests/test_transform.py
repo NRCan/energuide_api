cdf427b5b19fe55e4be112fbb16a971bdf977655
David Buckley <buckley.w.david@gmail.com>
2018-01-25 16:11:30 -0500

    create cleaning and transformation functions needed to transform input into expected output

--- python/tests/test_transform.py
+++ python/tests/test_transform.py
@@ -6,44 +6,107 @@ import pytest
 from energuide import database
 from energuide import transform
 
-
-def test_clean() -> None:
-    dataframe = pd.DataFrame.from_dict({'foo': [np.NaN]}, orient='columns')
-    output = transform.clean(dataframe).to_dict('records')
-
-    expected = [{'foo': None}]
-    assert output == expected
-
-
-def test_run(database_coordinates: database.DatabaseCoordinates,
-             mongo_client: pymongo.MongoClient,
-             database_name: str,
-             collection: str,
-             energuide_fixture: str) -> None:
-
-    transform.run(database_coordinates, database_name, collection, energuide_fixture)
-    assert mongo_client[database_name][collection].count() == 3
-
-
 @pytest.fixture
 def sample() -> pd.DataFrame:
     data = {
-        'EVAL_ID': 123456,
-        'IDNUMBER': 23,
-        'CREATIONDATE': '2009-01-01 12:01:02',
-        'MODIFICATIONDATE': '2011-01-01 00:01:02',
-        'YEARBUILT': 1979,
-        'HOUSEREGION': 'Ontario',
-        'CLIENTCITY': 'Kingston',
-        'CLIENTPCODE': 'K0H 1Y0',
+        'EVAL_ID': [123456],
+        'IDNUMBER': [23],
+        'CREATIONDATE': ['2009-01-01 12:01:02'],
+        'MODIFICATIONDATE': ['2011-01-01 00:01:02'],
+        'YEARBUILT': [1979],
+        'HOUSEREGION': ['Ontario'],
+        'CLIENTCITY': ['Kingston'],
+        'CLIENTPCODE': ['K0H 1Y0'],
     }
     return pd.DataFrame.from_dict(data)
+
+@pytest.fixture()
+def renamed_sample(sample: pd.DataFrame) -> pd.DataFrame:
+    return transform.rename_columns(sample)
+
+
+def test_clear_blanks() -> None:
+    dataframe = pd.DataFrame.from_dict({'foo': [np.NaN], 'bar': ['some']}, orient='columns')
+    output = transform.clear_blanks(dataframe).to_dict('records')
+
+    expected = [{'foo': None, 'bar': 'some'}]
+    assert output == expected
+
+
+def test_rename_columns(sample: pd.DataFrame)  -> None:
+    dataframe = transform.rename_columns(sample)
+    expected = [
+        'clientCity',
+        'clientPostalCode',
+        'creationDate',
+        'evalId',
+        'houseRegion',
+        'idNumber',
+        'modificationDate',
+        'yearBuilt'
+    ]
+
+    assert sorted(list(dataframe.columns)) == expected
+
+
+def test_extract_postal(renamed_sample: pd.DataFrame)  -> None:
+    output = transform.extract_postal(renamed_sample).to_dict('records')
+
+    expected = [{
+        'evalId': 123456,
+        'idNumber': 23,
+        'yearBuilt': 1979,
+        'houseRegion': 'Ontario',
+        'clientCity': 'Kingston',
+        'clientForwardSortationArea': 'K0H',
+        'clientPostalCode': 'K0H 1Y0',
+        'creationDate': '2009-01-01 12:01:02',
+        'modificationDate': '2011-01-01 00:01:02'
+    }]
+
+    assert output == expected
+
+
+def test_parse_dates(renamed_sample: pd.DataFrame)  -> None:
+    output = transform.parse_dates(renamed_sample).to_dict('records')
+
+    expected = [{
+        'evalId': 123456,
+        'idNumber': 23,
+        'yearBuilt': 1979,
+        'houseRegion': 'Ontario',
+        'clientCity': 'Kingston',
+        'clientPostalCode': 'K0H 1Y0',
+        'creationDate': datetime.datetime(2009, 1, 1, 12, 1, 2),
+        'modificationDate': datetime.datetime(2011, 1, 1, 0, 1, 2),
+    }]
+
+    assert output == expected
+
+
+def test_group_dates(renamed_sample: pd.DataFrame)  -> None:
+    output = transform.group_dates(renamed_sample).to_dict('records')
+
+    expected = [{
+        'evalId': 123456,
+        'idNumber': 23,
+        'yearBuilt': 1979,
+        'houseRegion': 'Ontario',
+        'clientCity': 'Kingston',
+        'clientPostalCode': 'K0H 1Y0',
+        'evaluations': [{
+            'creationDate': '2009-01-01 12:01:02',
+            'modificationDate': '2011-01-01 00:01:02'
+        }]
+    }]
+
+    assert output == expected
 
 
 def test_clean_with_sample(sample: pd.DataFrame) -> None:
     output = transform.clean(sample).to_dict('records')
 
-    expected = {
+    expected = [{
         'evalId': 123456,
         'idNumber': 23,
         'yearBuilt': 1979,
@@ -57,5 +120,15 @@ def test_clean_with_sample(sample: pd.DataFrame) -> None:
                 'modificationDate': datetime.datetime(2011, 1, 1, 0, 1, 2),
             }
         ]
-    }
+    }]
     assert output == expected
+
+
+def test_run(database_coordinates: database.DatabaseCoordinates,
+             mongo_client: pymongo.MongoClient,
+             database_name: str,
+             collection: str,
+             energuide_fixture: str) -> None:
+
+    transform.run(database_coordinates, database_name, collection, energuide_fixture)
+    assert mongo_client[database_name][collection].count() == 3
