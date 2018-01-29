486ea578aa154fac7008029082001dae7b1fe609
Jason White <jason.white@cds-snc.ca>
2018-01-29 16:28:31 -0500

    add new grouped_load command

--- python/src/energuide/database.py
+++ python/src/energuide/database.py
@@ -6,6 +6,8 @@ import os
 import pymongo
 import pandas as pd
 
+from energuide import dwelling
+
 
 class EnvVariables(enum.Enum):
     username = 'ENERGUIDE_USERNAME'
@@ -66,3 +68,38 @@ def load(coords: DatabaseCoordinates,
         collection = database[collection_name]
 
         collection.insert_many(dataframe.to_dict('records'))
+
+
+_DEFAULT_CHUNK_SIZE = 1000
+
+
+def _chunk_data(data: typing.Iterable[dwelling.Dwelling],
+                max_chunk_size: int = _DEFAULT_CHUNK_SIZE
+               ) -> typing.Iterator[typing.List[typing.Dict[str, typing.Any]]]:
+
+    chunked_list: typing.List[typing.Dict[str, typing.Any]] = []
+    load_size = 0
+
+    for row in data:
+        if load_size >= max_chunk_size:
+            yield chunked_list
+            chunked_list = []
+            load_size = 0
+        chunked_list.append(row.to_dict())
+        load_size += 1
+
+    if chunked_list:
+        yield chunked_list
+
+
+def grouped_load(coords: DatabaseCoordinates,
+                 database_name: str,
+                 collection_name: str,
+                 data: typing.Iterable[dwelling.Dwelling]) -> None:
+    client: pymongo.MongoClient
+    with mongo_client(coords) as client:
+        database = client[database_name]
+        collection = database[collection_name]
+
+        for data_to_load in _chunk_data(data):
+            collection.insert_many(data_to_load)
