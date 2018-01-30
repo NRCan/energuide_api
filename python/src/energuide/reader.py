72ce5cf4b11cf160af2112859c6b31c5ba67c506
David Buckley <buckley.w.david@gmail.com>
2018-01-30 15:52:11 -0500

    add start of extract

--- python/src/energuide/reader.py
+++ python/src/energuide/reader.py
@@ -1,14 +1,29 @@
 import csv
+import json
 import itertools
 import sys
 import typing
 
 
 InputData = typing.Dict[str, typing.Any]
-
+                
 
 class InvalidInputDataException(Exception):
     pass
+
+
+_SCHEMA = {
+        'EVAL_ID': {'type': 'integer', 'required': True, 'coerce': int},
+        'EVAL_TYPE': {'type': 'string', 'required': True, 'allowed': [eval_type.value for eval_type in EvaluationType]},
+        'ENTRYDATE': {'type': 'string', 'required': True},
+        'CREATIONDATE': {'type': 'string', 'required': True},
+        'MODIFICATIONDATE': {'type': 'string', 'required': True},
+        'YEARBUILT': {'type': 'integer', 'required': True, 'coerce': int},
+        'CLIENTCITY': {'type': 'string', 'required': True},
+        'CLIENTPCODE': {'type': 'string', 'required': True, 'regex': '[A-Z][0-9][A-Z] [0-9][A-Z][0-9]'},
+        'HOUSEREGION': {'type': 'string', 'required': True},
+    }
+
 
 
 def read(filename: str) -> typing.Iterator[InputData]:
@@ -17,6 +32,22 @@ def read(filename: str) -> typing.Iterator[InputData]:
         reader = csv.DictReader(csvfile)
         for row in reader:
             yield row
+
+def extract_xml(xml: str):
+    return {'testfield': 0}
+
+
+def extract(infile: str, outfile: str):
+    validator = cerberus.Validator(_SCHEMA, allow_unknown=True)
+    input_data = read(infile)
+    with open(outfile, 'w') as output:
+        writer = csv.DictWriter(output, fieldnames)
+        for row in input_data:
+            xml = row.pop('xml')
+
+
+            writer.writerow()
+
 
 
 def grouper(raw: typing.Iterable[InputData],
