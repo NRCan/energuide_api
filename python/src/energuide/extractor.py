b3faf6a0e673baaa67aa5bb0b84baefd23df865e
David Buckley <buckley.w.david@gmail.com>
2018-01-30 17:01:29 -0500

    change strategy to keep known fields and drop specific known ones

--- python/src/energuide/extractor.py
+++ python/src/energuide/extractor.py
@@ -1,30 +1,73 @@
 import csv
+import itertools
 import json
 import cerberus
 from energuide import reader, dwelling
 
-# TODO reference schema from somewhere else, possibly dwelling.ParsedDwellingDataRow
+
+class SerializableGenerator(list):
+    def __init__(self, iterable):
+        tmp_body = iter(iterable)
+        try:
+            self._head = iter([next(tmp_body)])
+            self.append(tmp_body)
+        except StopIteration:
+            self._head = []
+
+    def __iter__(self):
+        return itertools.chain(self._head, *self[:1])
+
+
+
+
 _SCHEMA = {
         'EVAL_ID': {'type': 'integer', 'required': True, 'coerce': int},
         'EVAL_TYPE': {'type': 'string', 'required': True, 'allowed': [eval_type.value for eval_type in dwelling.EvaluationType]},
-        'ENTRYDATE': {'type': 'string', 'required': True},
-        'CREATIONDATE': {'type': 'string', 'required': True},
-        'MODIFICATIONDATE': {'type': 'string', 'required': True},
-        'YEARBUILT': {'type': 'integer', 'required': True, 'coerce': int},
-        'CLIENTCITY': {'type': 'string', 'required': True},
-        'CLIENTPCODE': {'type': 'string', 'required': True, 'regex': '[A-Z][0-9][A-Z] [0-9][A-Z][0-9]'},
-        'HOUSEREGION': {'type': 'string', 'required': True},
+
+        'HOUSEREGION': {'type': 'integer', 'required': True},
+        'WEATHERLOC': {'type': 'integer', 'required': True},
+        'ENTRYBY': {'type': 'integer', 'required': True},
+        'CLIENTCITY': {'type': 'integer', 'required': True},
+        'CLIENTPCODE': {'type': 'integer', 'required': True},
+        'CLIENTNAME': {'type': 'integer', 'required': True},
+        'TELEPHONE': {'type': 'integer', 'required': True},
+        'MAIL_CITY': {'type': 'integer', 'required': True},
+        'MAIL_REGION': {'type': 'integer', 'required': True},
+        'MAIL_PCODE': {'type': 'integer', 'required': True},
+        'TAXNUMBER': {'type': 'integer', 'required': True},
+
+        'RAW_XML': {'type': 'string', 'required': True}
     }
 
-_fields = _SCHEMA.keys()
+_DROP = ['HOUSEREGION',
+         'WEATHERLOC',
+         'ENTRYBY', 
+         'CLIENTCITY',
+         'CLIENTPCODE'
+         'CLIENTNAME',
+         'TELEPHONE', 
+         'MAIL_CITY', 
+         'MAIL_REGION'
+         'MAIL_PCODE',
+         'TAXNUMBER', 
+         'RAW_XML']
+
+def validated(it: typing.Iterable[reader.InputData]) -> typing.Iterator[reader.InputData]:
+    for row in reader:
+        if not validator.validate(row):
+            error_keys = ', '.join(validator.errors.keys())
+            raise reader.InvalidInputDataException(f'Validator failed on keys: {error_keys}')
+        document = validator.document
+
+        for key in _DROP:
+            document.pop(key)
+
+        yield document
 
 
-def extract(infile: str, outfile: str):
-    validator = cerberus.Validator(_SCHEMA, allow_unknown=True, purge_unknown=True)
-    with open(infile, 'r') as csv_input, open(outfile, 'w') as csv_output:
+
+def extract(infile: str) ->. typing.Iterator[reader.InputData]:
+    validator = cerberus.Validator(_SCHEMA, allow_unknown=True)
+    with open(infile, 'r') as csv_input, open(outfile, 'w') as json_output:
         reader = csv.DictReader(csv_input)
-        writer = csv.DictWriter(csv_output, fieldnames=[field for field in reader.fieldnames if field in _fields])
-
-        out = (blob for blob in (validator.validated(row) for row in input_data)
-               if blob is not None)
-        json.dump(out)
+        return SerializableGenerator(blob for blob in validated(reader))
--- python/tests/test_extractor.py
+++ python/tests/test_extractor.py
@@ -1,17 +1,16 @@
+import json
 import typing
 import os
 import pytest
 from energuide import extractor
 
-@pytest.fixture
-def extract_out() -> str:
-    path =  os.path.join(os.path.dirname(__file__), 'extractor_out.json')
-    yield path
-    os.remove(path)
 
-def test_extract(energuide_fixture: str, extract_out: str):
-    extractor.extract(energuide_fixture, extract_out)
-    with open(extract_out, 'r'):
-        print(extract_out.read())
+def test_extract(energuide_fixture: str):
+    output = extractor.extract(energuide_fixture, extract_out)
+
+    for row in output:
+        print(row)
+
+    assert False
 
 
