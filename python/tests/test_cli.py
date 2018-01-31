ac687868595588585bd0137e2292b778e9e11b0e
David Buckley <buckley.w.david@gmail.com>
2018-01-31 15:20:03 -0500

    change extractor to output a zip of json files instead of one json file

--- python/tests/test_cli.py
+++ python/tests/test_cli.py
@@ -1,4 +1,4 @@
-import os
+import zipfile
 import py
 import _pytest
 from click import testing  # type: ignore
@@ -9,11 +9,11 @@ from energuide import cli
 
 @pytest.fixture(params=[
     '''EVAL_ID,EVAL_TYPE,ENTRYBY,CLIENTADDR,CLIENTPCODE,CLIENTNAME,TELEPHONE,MAIL_ADDR,\
-MAIL_PCODE,TAXNUMBER,RAW_XML\n123,D,Fred Johnson,123 Main st.,M5E 1W5,John \
-Fredson,999 999 9999,123 Main st.,M5E 1W5,999999999999,<tag>thing</tag>''',
+MAIL_PCODE,TAXNUMBER,RAW_XML,BUILDER\n123,D,Fred Johnson,123 Main st.,M5E 1W5,John \
+Fredson,999 999 9999,123 Main st.,M5E 1W5,999999999999,<tag>thing</tag>,4K13D01404''',
     '''EVAL_ID,EVAL_TYPE,ENTRYBY,CLIENTADDR,CLIENTPCODE,CLIENTNAME,TELEPHONE,MAIL_ADDR,\
-MAIL_PCODE,TAXNUMBER,RAW_XML,other_1,other_2\n123,D,Fred Johnson,123 Main st.,M5E 1W5,John \
-Fredson,999 999 9999,123 Main st.,M5E 1W5,999999999999,<tag>thing</tag>,foo,bar'''])
+MAIL_PCODE,TAXNUMBER,RAW_XML,BUILDER,other_1,other_2\n123,D,Fred Johnson,123 Main st.,M5E 1W5,John \
+Fredson,999 999 9999,123 Main st.,M5E 1W5,999999999999,<tag>thing</tag>,4K02E90020,foo,bar'''])
 def passing_str(request: _pytest.fixtures.SubRequest) -> str:
     return request.param
 
@@ -31,7 +31,7 @@ def valid_filepath(tmpdir: py._path.local.LocalPath, passing_str: str) -> str:
 def invalid_filepath(tmpdir: py._path.local.LocalPath) -> str:
     filepath = f'{tmpdir}/sample.csv'
     with open(filepath, 'w') as file:
-        file.write('EVAL_ID,EVAL_TYPE\nfoo,bar')
+        file.write('EVAL_ID,EVAL_TYPE,BUILDER\nfoo,bar,\nfoo,bar,4K02E90020')
 
     return filepath
 
@@ -51,7 +51,7 @@ def test_load(energuide_fixture: str, database_name: str, collection: str, mongo
 
 
 def test_extract_valid(valid_filepath: str, tmpdir: py._path.local.LocalPath) -> None:
-    outfile = f'{tmpdir}/output.json'
+    outfile = f'{tmpdir}/output.zip'
     runner = testing.CliRunner()
     result = runner.invoke(cli.main, args=[
         'extract',
@@ -60,11 +60,13 @@ def test_extract_valid(valid_filepath: str, tmpdir: py._path.local.LocalPath) ->
     ])
 
     assert result.exit_code == 0
-    assert os.path.isfile(outfile)
+
+    with zipfile.ZipFile(outfile, 'r') as output:
+        assert len(output.namelist()) == 1
 
 
 def test_extract_invalid(invalid_filepath: str, tmpdir: py._path.local.LocalPath) -> None:
-    outfile = f'{tmpdir}/output.json'
+    outfile = f'{tmpdir}/output.zip'
     runner = testing.CliRunner()
     result = runner.invoke(cli.main, args=[
         'extract',
@@ -73,11 +75,13 @@ def test_extract_invalid(invalid_filepath: str, tmpdir: py._path.local.LocalPath
     ])
 
     assert result.exit_code != 0
-    assert not os.path.isfile(outfile)
+
+    with zipfile.ZipFile(outfile, 'r') as output:
+        assert not output.namelist()
 
 
 def test_extract_missing(tmpdir: py._path.local.LocalPath) -> None:
-    outfile = f'{tmpdir}/output.json'
+    outfile = f'{tmpdir}/output.zip'
     infile = f'{tmpdir}/idontexist.csv'
     runner = testing.CliRunner()
     result = runner.invoke(cli.main, args=[
@@ -87,4 +91,6 @@ def test_extract_missing(tmpdir: py._path.local.LocalPath) -> None:
     ])
 
     assert result.exit_code != 0
-    assert not os.path.isfile(outfile)
+
+    with zipfile.ZipFile(outfile, 'r') as output:
+        assert not output.namelist()
