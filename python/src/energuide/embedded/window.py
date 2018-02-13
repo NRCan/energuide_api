9bc9e42004d9e222e1d8e488f6e6e27310427a1f
Steve Astels <stephen.astels@cds-snc.ca>
2018-02-13 10:07:52 -0500

    refactor Window

---
+++ python/src/energuide/embedded/window.py
@@ -0,0 +1,64 @@
+import typing
+from energuide.embedded import area
+from energuide.embedded import code
+from energuide.embedded import insulation
+from energuide.embedded import distance
+from energuide import element
+
+_MILLIMETRES_TO_METRES = 1000
+
+
+class _Window(typing.NamedTuple):
+    label: str
+    window_code: typing.Optional[code.WindowCode]
+    insulation: insulation.Insulation
+    width: distance.Distance
+    height: distance.Distance
+
+
+class Window(_Window):
+
+    @classmethod
+    def from_data(cls,
+                  window: element.Element,
+                  window_codes: typing.Dict[str, code.WindowCode]) -> 'Window':
+
+        code_id = window.xpath('Construction/Type/@idref')
+        window_code = window_codes[code_id[0]] if code_id else None
+
+        return Window(
+            label=window.get_text('Label'),
+            window_code=window_code,
+            insulation=insulation.Insulation(float(window.xpath('Construction/Type/@rValue')[0])),
+            width=distance.Distance(float(window.xpath('Measurements/@width')[0]) / _MILLIMETRES_TO_METRES),
+            height=distance.Distance(float(window.xpath('Measurements/@height')[0]) / _MILLIMETRES_TO_METRES),
+        )
+
+    @property
+    def _window_area(self) -> area.Area:
+        return area.Area(self.width.metres * self.height.metres)
+
+    def to_dict(self) -> typing.Dict[str, typing.Any]:
+        return {
+            'label': self.label,
+            'rsi': self.insulation.rsi,
+            'rvalue': self.insulation.r_value,
+            'glazingTypesEnglish': self.window_code.glazing_type.english if self.window_code else None,
+            'glazingTypesFrench': self.window_code.glazing_type.french if self.window_code else None,
+            'coatingsTintsEnglish': self.window_code.coating_tint.english if self.window_code else None,
+            'coatingsTintsFrench': self.window_code.coating_tint.french if self.window_code else None,
+            'fillTypeEnglish': self.window_code.fill_type.english if self.window_code else None,
+            'fillTypeFrench': self.window_code.fill_type.french if self.window_code else None,
+            'spacerTypeEnglish': self.window_code.spacer_type.english if self.window_code else None,
+            'spacerTypeFrench': self.window_code.spacer_type.french if self.window_code else None,
+            'typeEnglish': self.window_code.window_code_type.english if self.window_code else None,
+            'typeFrench': self.window_code.window_code_type.french if self.window_code else None,
+            'frameMaterialEnglish': self.window_code.frame_material.english if self.window_code else None,
+            'frameMaterialFrench': self.window_code.frame_material.french if self.window_code else None,
+            'areaMetres': self._window_area.square_metres,
+            'areaFeet': self._window_area.square_feet,
+            'widthMetres': self.width.metres,
+            'widthFeet': self.width.feet,
+            'heightMetres': self.height.metres,
+            'heightFeet': self.height.feet,
+        }
