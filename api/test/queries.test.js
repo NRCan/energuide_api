b14500c009d122eaf983624f97425fa2cce39d97
Mike Williamson <mike.williamson@tbs-sct.gc.ca>
2018-02-13 18:55:45 -0500

    Add integration tests that fail when data changes
    These integration tests assert against the actual values currently being
    returned by the fixture data. If the data shifts, and the values change
    (or are null) these tests will fail and stop the build/deployment
    process.

--- api/test/queries.test.js
+++ api/test/queries.test.js
@@ -20,6 +20,351 @@ describe('queries', () => {
     client.close()
   })
 
+  describe('Dwelling data', () => {
+    it('retrieves all top level keys of the dwelling data', async () => {
+      let response = await request(server)
+        .post('/graphql')
+        .set('Content-Type', 'application/json; charset=utf-8')
+        .send({
+          query: `{
+          evaluations:evaluationsFor(account: 189250) {
+              houseId
+              yearBuilt
+              city
+              region
+              forwardSortationArea
+            }
+        }`,
+        })
+
+      let { evaluations } = response.body.data
+      expect(evaluations).toEqual({
+        city: 'Charlottetown',
+        forwardSortationArea: 'C1A',
+        houseId: 189250,
+        region: 'PE',
+        yearBuilt: 1900,
+      })
+    })
+
+    it('retrieves all top level keys of the evaluation data', async () => {
+      let response = await request(server)
+        .post('/graphql')
+        .set('Content-Type', 'application/json; charset=utf-8')
+        .send({
+          query: `{
+          evaluations:evaluationsFor(account: 189250) {
+            evaluations {
+              evaluationType
+              entryDate
+              creationDate
+              modificationDate
+            }
+          }
+        }`,
+        })
+
+      let { evaluations } = response.body.data
+      expect(evaluations).toEqual({
+        evaluations: [
+          {
+            creationDate: '2012-10-01T15:08:41',
+            entryDate: '2011-11-18',
+            evaluationType: 'E',
+            modificationDate: '2012-06-09T11:20:20',
+          },
+          {
+            creationDate: '2012-10-01T15:08:39',
+            entryDate: '2011-08-18',
+            evaluationType: 'D',
+            modificationDate: '2012-06-09T11:20:20',
+          },
+        ],
+      })
+    })
+
+    it('retrieves all top level keys of the ceiling data', async () => {
+      let response = await request(server)
+        .post('/graphql')
+        .set('Content-Type', 'application/json; charset=utf-8')
+        .send({
+          query: `{
+          evaluations:evaluationsFor(account: 189250) {
+            evaluations {
+              ceilings {
+                label
+                typeEnglish
+                typeFrench
+                nominalRsi
+                nominalR
+                effectiveRsi
+                effectiveR
+                areaMetres
+                areaFeet
+                lengthMetres
+                lengthFeet
+              }
+            }
+          }
+        }`,
+        })
+
+      let { evaluations } = response.body.data
+      expect(evaluations).toEqual({
+        evaluations: [
+          {
+            ceilings: [
+              {
+                areaFeet: 830.1171300283143,
+                areaMetres: 77.1204,
+                effectiveR: 36.3397497041326,
+                effectiveRsi: 6.3998,
+                label: 'Ceiling01',
+                lengthFeet: 47.66404352,
+                lengthMetres: 14.528,
+                nominalR: 36.3397497041326,
+                nominalRsi: 6.3998,
+                typeEnglish: 'Attic/gable',
+                typeFrench: 'Combles/pignon',
+              },
+            ],
+          },
+          {
+            ceilings: [
+              {
+                areaFeet: 830.1171300283143,
+                areaMetres: 77.1204,
+                effectiveR: 36.3397497041326,
+                effectiveRsi: 6.3998,
+                label: 'Ceiling01',
+                lengthFeet: 47.66404352,
+                lengthMetres: 14.528,
+                nominalR: 36.3397497041326,
+                nominalRsi: 6.3998,
+                typeEnglish: 'Attic/gable',
+                typeFrench: 'Combles/pignon',
+              },
+            ],
+          },
+        ],
+      })
+    })
+
+    it('retrieves all top level keys of the wall data', async () => {
+      let response = await request(server)
+        .post('/graphql')
+        .set('Content-Type', 'application/json; charset=utf-8')
+        .send({
+          query: `{
+          evaluations:evaluationsFor(account: 189250) {
+            evaluations {
+              walls {
+                label
+                structureTypeEnglish
+                structureTypeFrench
+                componentTypeSizeEnglish
+                componentTypeSizeFrench
+                nominalRsi
+                nominalR
+                effectiveRsi
+                effectiveR
+                areaMetres
+                areaFeet
+                perimeter
+                height
+              }
+            }
+          }
+        }`,
+        })
+
+      let { evaluations } = response.body.data
+      expect(evaluations).toEqual({
+        evaluations: [
+          {
+            walls: [
+              {
+                areaFeet: 1000.7916833561255,
+                areaMetres: 92.97658384,
+                componentTypeSizeEnglish: null,
+                componentTypeSizeFrench: null,
+                effectiveR: 15.2699857658604,
+                effectiveRsi: 2.6892,
+                height: 2.5999,
+                label: 'Main floor',
+                nominalR: 19.0874822073255,
+                nominalRsi: 3.3615,
+                perimeter: 35.7616,
+                structureTypeEnglish: null,
+                structureTypeFrench: null,
+              },
+            ],
+          },
+          {
+            walls: [
+              {
+                areaFeet: 1000.7916833561255,
+                areaMetres: 92.97658384,
+                componentTypeSizeEnglish: null,
+                componentTypeSizeFrench: null,
+                effectiveR: 15.2699857658604,
+                effectiveRsi: 2.6892,
+                height: 2.5999,
+                label: 'Main floor',
+                nominalR: 19.0874822073255,
+                nominalRsi: 3.3615,
+                perimeter: 35.7616,
+                structureTypeEnglish: null,
+                structureTypeFrench: null,
+              },
+            ],
+          },
+        ],
+      })
+    })
+
+    it('retrieves all top level keys of the door data', async () => {
+      let response = await request(server)
+        .post('/graphql')
+        .set('Content-Type', 'application/json; charset=utf-8')
+        .send({
+          query: `{
+          evaluations:evaluationsFor(account: 189250) {
+            evaluations {
+              doors {
+                typeEnglish
+                typeFrench
+                rsi
+                rValue
+                uFactor
+                uFactorImperial
+                areaMetres
+                areaFeet
+              }
+            }
+          }
+        }`,
+        })
+
+      let { evaluations } = response.body.data
+      expect(evaluations).toEqual({
+        evaluations: [
+          {
+            doors: [
+              {
+                areaFeet: 18.9875391902784,
+                areaMetres: 1.764,
+                rValue: 6.4732202041799995,
+                rsi: 1.14,
+                typeEnglish: 'Steel Medium density spray foam core',
+                typeFrench:
+                  'Acier / âme en mousse à vaporiser de densité moyenne',
+                uFactor: 0.8771929824561404,
+                uFactorImperial: 0.15448261737709199,
+              },
+              {
+                areaFeet: 18.9875391902784,
+                areaMetres: 1.764,
+                rValue: 6.4732202041799995,
+                rsi: 1.14,
+                typeEnglish: 'Steel Medium density spray foam core',
+                typeFrench:
+                  'Acier / âme en mousse à vaporiser de densité moyenne',
+                uFactor: 0.8771929824561404,
+                uFactorImperial: 0.15448261737709199,
+              },
+            ],
+          },
+          {
+            doors: [
+              {
+                areaFeet: 18.9875391902784,
+                areaMetres: 1.764,
+                rValue: 6.4732202041799995,
+                rsi: 1.14,
+                typeEnglish: 'Steel Medium density spray foam core',
+                typeFrench:
+                  'Acier / âme en mousse à vaporiser de densité moyenne',
+                uFactor: 0.8771929824561404,
+                uFactorImperial: 0.15448261737709199,
+              },
+              {
+                areaFeet: 18.9875391902784,
+                areaMetres: 1.764,
+                rValue: 6.4732202041799995,
+                rsi: 1.14,
+                typeEnglish: 'Steel Medium density spray foam core',
+                typeFrench:
+                  'Acier / âme en mousse à vaporiser de densité moyenne',
+                uFactor: 0.8771929824561404,
+                uFactorImperial: 0.15448261737709199,
+              },
+            ],
+          },
+        ],
+      })
+    })
+
+    it('retrieves all top level keys of the window data', async () => {
+      let response = await request(server)
+        .post('/graphql')
+        .set('Content-Type', 'application/json; charset=utf-8')
+        .send({
+          query: `{
+          evaluations:evaluationsFor(account: 189250) {
+            evaluations {
+              windows {
+                label
+                rsi
+                rvalue
+                glazingTypesEnglish
+                glazingTypesFrench
+                coatingsTintsEnglish
+                coatingsTintsFrench
+                fillTypeEnglish
+                fillTypeFrench
+                spacerTypeEnglish
+                spacerTypeFrench
+                typeEnglish
+                typeFrench
+                frameMaterialEnglish
+                frameMaterialFrench
+                areaMetres
+                areaFeet
+                width
+                height
+              }
+            }
+          }
+        }`,
+        })
+
+      let { evaluations: { evaluations: [first] } } = response.body.data
+      let [eastWindow] = first.windows
+      expect(eastWindow).toEqual({
+        label: 'East0001',
+        rsi: 0.2685,
+        rvalue: 1.5246137059845,
+        glazingTypesEnglish: 'Double/double with 1 coat',
+        glazingTypesFrench: 'Double/double, 1 couche',
+        coatingsTintsEnglish: 'Clear',
+        coatingsTintsFrench: 'Transparent',
+        fillTypeEnglish: '13 mm Air',
+        fillTypeFrench: "13 mm d'air",
+        spacerTypeEnglish: 'Metal',
+        spacerTypeFrench: 'Métal',
+        typeEnglish: 'Picture',
+        typeFrench: 'Fixe',
+        frameMaterialEnglish: 'Aluminum',
+        frameMaterialFrench: 'Aluminium',
+        areaMetres: 1.31104735596684,
+        areaFeet: 14.111997194858986,
+        width: 1.0746954,
+        height: 1.2199246000000001,
+      })
+    })
+  })
+
   describe('evaluationsFor', () => {
     it('retrieves evaluations given an account id and a postalcode', async () => {
       let response = await request(server)
