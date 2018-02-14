ee07eb99142bda0b75b3b5b02252810a8007218b
Paul Craig <paul_craig_16@hotmail.com>
2018-02-14 17:36:34 -0500

    Prettier formatting for server.js file
    Doing this as a separate commit so that it's not confused with
    the actual change I want to make.

--- api/src/server.js
+++ api/src/server.js
@@ -3,10 +3,10 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 import Schema from './schema'
-import { i18n , unpackCatalog } from 'lingui-i18n'
+import { i18n, unpackCatalog } from 'lingui-i18n'
 import requestLanguage from 'express-request-language'
 
-i18n.load({ 
+i18n.load({
   fr: unpackCatalog(require('./locale/fr/messages.js')),
   en: unpackCatalog(require('./locale/en/messages.js')),
 })
@@ -15,25 +15,25 @@ function Server(context = {}, ...middlewares) {
   const server = express()
   middlewares.forEach(middleware => server.use(middleware))
   server
-  .use(
-    requestLanguage({
-      languages: i18n.availableLanguages.sort(),
-    }),
-  )
-  .use(cors())
-  .use(
-    '/graphql',
-    bodyParser.json(),
-    graphqlExpress(request => {
-      i18n.activate(request.language)
-      return {
-        schema: new Schema(i18n),
-        context,
-        tracing: true,
-        cacheControl: true,
-      }
-    }),
-  )
+    .use(
+      requestLanguage({
+        languages: i18n.availableLanguages.sort(),
+      }),
+    )
+    .use(cors())
+    .use(
+      '/graphql',
+      bodyParser.json(),
+      graphqlExpress(request => {
+        i18n.activate(request.language)
+        return {
+          schema: new Schema(i18n),
+          context,
+          tracing: true,
+          cacheControl: true,
+        }
+      }),
+    )
   server.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
   return server
 }
