5482ffe2b98828db4c1d343ca4f24057ed8fe10c
Steve Astels <stephen.astels@cds-snc.ca>
2018-02-15 14:56:30 -0500

    added pytest fixtures

--- python/tests/test_extract_endpoints.py
+++ python/tests/test_extract_endpoints.py
@@ -1,16 +1,52 @@
 import os
-import shutil
 from io import BytesIO
-from extract_endpoint import extract_endpoint
-from azure_storage import EnvVariables, EnvDefaults
+import pytest
 from azure.storage.blob import BlockBlobService
-
-
-CONTENTS = 'test_contents'
-TEST_FILENAME = 'test_blob_filename_989.txt'
-
+from azure_storage import EnvVariables, EnvDefaults, StorageCoordinates
+from extract_endpoint import extract_endpoint
 
 extract_endpoint.App.testing = True
+
+
+@pytest.fixture
+def test_file_contents() -> str:
+    return 'test_contents'
+
+
+@pytest.fixture
+def test_file_name() -> str:
+    return 'test_blob_filename_989.txt'
+
+
+@pytest.fixture
+def account() -> str:
+    return os.environ.get(EnvVariables.account.value, EnvDefaults.account.value)
+
+
+@pytest.fixture
+def key() -> str:
+    return os.environ.get(EnvVariables.key.value, EnvDefaults.key.value)
+
+
+@pytest.fixture
+def container() -> str:
+    return os.environ.get(EnvVariables.container.value, EnvDefaults.container.value)
+
+
+@pytest.fixture
+def domain() -> str:
+    return os.environ.get(EnvVariables.domain.value, EnvDefaults.domain.value)
+
+
+@pytest.fixture
+def storage_coordinates(account: str, key: str, container: str, domain: str) -> StorageCoordinates:
+    return StorageCoordinates(account=account, key=key, container=container, domain=domain)
+
+
+@pytest.fixture
+def block_blob_service(storage_coordinates: StorageCoordinates) -> BlockBlobService:
+    account, key, _, domain = storage_coordinates
+    return BlockBlobService(account_name=account, account_key=key, custom_domain=domain)
 
 
 def test_frontend() -> None:
@@ -19,31 +55,27 @@ def test_frontend() -> None:
     assert b'Upload new File' in actual.data
 
 
-def test_upload_no_key() -> None:
+def test_upload_no_key(test_file_contents: str, test_file_name: str) -> None:
     app = extract_endpoint.App.test_client()
-    stream = BytesIO(CONTENTS.encode('utf-8'))
-    actual = app.post('/upload_file', data=dict(file=(stream, TEST_FILENAME)))
+    stream = BytesIO(test_file_contents.encode('utf-8'))
+    actual = app.post('/upload_file', data=dict(file=(stream, test_file_name)))
     assert actual.status_code == 404
 
 
-def test_upload_wrong_key() -> None:
+def test_upload_wrong_key(test_file_contents: str, test_file_name: str) -> None:
     app = extract_endpoint.App.test_client()
-    stream = BytesIO(CONTENTS.encode('utf-8'))
-    actual = app.post('/upload_file', data=dict(key='bad key', file=(stream, TEST_FILENAME)))
+    stream = BytesIO(test_file_contents.encode('utf-8'))
+    actual = app.post('/upload_file', data=dict(key='bad key', file=(stream, test_file_name)))
     assert actual.status_code == 404
 
 
-def test_upload() -> None:
+def test_upload(block_blob_service: BlockBlobService, container: str,
+                test_file_contents: str, test_file_name: str) -> None:
     app = extract_endpoint.App.test_client()
-    stream = BytesIO(CONTENTS.encode('utf-8'))
-    app.post('/upload_file', data=dict(key='development_key', file=(stream, TEST_FILENAME)))
+    stream = BytesIO(test_file_contents.encode('utf-8'))
+    app.post('/upload_file', data=dict(key='development_key', file=(stream, test_file_name)))
 
-    account = os.environ.get(EnvVariables.account.value, EnvDefaults.account.value)
-    key = os.environ.get(EnvVariables.key.value, EnvDefaults.key.value)
-    container = os.environ.get(EnvVariables.container.value, EnvDefaults.container.value)
-    domain = os.environ.get(EnvVariables.domain.value, EnvDefaults.domain.value)
-    block_blob_service = BlockBlobService(account_name=account, account_key=key, custom_domain=domain)
-    assert TEST_FILENAME in [blob.name for blob in block_blob_service.list_blobs(container)]
-    actual = block_blob_service.get_blob_to_text(container, TEST_FILENAME)
-    block_blob_service.delete_blob(container, TEST_FILENAME)
-    assert actual.content == CONTENTS
+    assert test_file_name in [blob.name for blob in block_blob_service.list_blobs(container)]
+    actual = block_blob_service.get_blob_to_text(container, test_file_name)
+    block_blob_service.delete_blob(container, test_file_name)
+    assert actual.content == test_file_contents
