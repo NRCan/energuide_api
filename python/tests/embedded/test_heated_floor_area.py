c7ee971e90088d9d8ad1e17fe7aa752b9aef3541
David Buckley <buckley.w.david@gmail.com>
2018-02-16 17:14:32 -0500

    HeatedFloorArea exceptions (#188)
    add HeatedFloorArea exception handling and tests

--- python/tests/embedded/test_heated_floor_area.py
+++ python/tests/embedded/test_heated_floor_area.py
@@ -1,6 +1,8 @@
 import pytest
 from energuide import element
 from energuide.embedded import heated_floor_area
+from energuide.exceptions import InvalidEmbeddedDataTypeException
+
 
 @pytest.fixture
 def sample() -> element.Element:
@@ -10,10 +12,32 @@ def sample() -> element.Element:
     return element.Element.from_string(doc)
 
 
+BAD_XML_DATA = [
+    # This XML block has non-numeric strings as attribute values
+    """
+    <HeatedFloorArea aboveGrade="bad" belowGrade="data" />
+    """,
+
+    # This XML block is missing the belowGrade attribute
+    """
+    <HeatedFloorArea aboveGrade="92.9" />
+    """
+]
+
+
 def test_from_data(sample: element.Element) -> None:
     output = heated_floor_area.HeatedFloorArea.from_data(sample)
     assert output.area_above_grade.square_metres == 92.9
     assert output.area_below_grade.square_feet == 1999.93468342048
+
+
+@pytest.mark.parametrize("bad_xml", BAD_XML_DATA)
+def test_bad_data(bad_xml: str) -> None:
+    floor_area_node = element.Element.from_string(bad_xml)
+    with pytest.raises(InvalidEmbeddedDataTypeException) as excinfo:
+        heated_floor_area.HeatedFloorArea.from_data(floor_area_node)
+
+    assert excinfo.value.data_class == heated_floor_area.HeatedFloorArea
 
 
 def test_to_dict(sample: element.Element) -> None:
