4eb1c15282ca7d10542d276cb906c54e9b5c98be
David Buckley <buckley.w.david@gmail.com>
2018-02-16 16:01:32 -0500

    Floor exceptions (#187)
    add floor exception handling and tests

--- python/tests/embedded/test_floor.py
+++ python/tests/embedded/test_floor.py
@@ -4,6 +4,7 @@ from energuide.embedded import area
 from energuide.embedded import distance
 from energuide.embedded import floor
 from energuide.embedded import insulation
+from energuide.exceptions import InvalidEmbeddedDataTypeException
 
 
 @pytest.fixture
@@ -18,6 +19,40 @@ def sample_raw() -> element.Element:
     </Floor>
     """
     return element.Element.from_string(doc)
+
+BAD_XML_DATA = [
+    # This XML block is missing the <Label> tag
+    """
+    <Floor>
+        <Construction>
+            <Type nominalInsulation='2.46' rValue='2.9181' />
+        </Construction>
+        <Measurements area='9.2903' length='3.048' />
+    </Floor>
+    """,
+
+    # This XML has non-numeric strings as attribute values
+    """
+    <Floor>
+        <Label>Rm over garage</Label>
+        <Construction>
+            <Type nominalInsulation='bad' rValue='data' />
+        </Construction>
+        <Measurements area='goes' length='here' />
+    </Floor>
+    """,
+
+    # This XML block is missing all attributes for <Type> and <Measurements> tags
+    """
+    <Floor>
+        <Label>Rm over garage</Label>
+        <Construction>
+            <Type />
+        </Construction>
+        <Measurements />
+    </Floor>
+    """,
+]
 
 
 @pytest.fixture
@@ -34,6 +69,15 @@ def sample() -> floor.Floor:
 def test_from_data(sample_raw: element.Element, sample: floor.Floor) -> None:
     output = floor.Floor.from_data(sample_raw)
     assert output == sample
+
+
+@pytest.mark.parametrize("bad_xml", BAD_XML_DATA)
+def test_bad_data(bad_xml: str) -> None:
+    floor_node = element.Element.from_string(bad_xml)
+    with pytest.raises(InvalidEmbeddedDataTypeException) as excinfo:
+        floor.Floor.from_data(floor_node)
+
+    assert excinfo.value.data_class == floor.Floor
 
 
 def test_to_dict(sample: floor.Floor) -> None:
