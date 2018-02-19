2cc297416c315659d242896dafd55fb3ed0bd8da
Steve Astels <sastels@gmail.com>
2018-02-19 15:54:22 -0500

    remove get_salt code

--- extract_endpoint/src/crypt_utils.py
+++ extract_endpoint/src/crypt_utils.py
@@ -1,13 +1,9 @@
-import secrets
 import hmac
-import base64
+import typing
 
 
-def get_salt(num_bytes=64) -> str:
-    return base64.b64encode(secrets.token_bytes(num_bytes)).decode('utf-8')
-
-
-def sign_string(salt: str, key: str, data: str) -> str:
+def sign_string(salt: typing.Optional[str], key: str, data: str) -> str:
+    salt = salt if salt is not None else ''
     hash_key = (salt + key).encode('utf-8')
     hashed_data = hmac.new(bytes(hash_key), data.encode('utf-8'))
     return hashed_data.hexdigest()
--- extract_endpoint/tests/test_crypt_utils.py
+++ extract_endpoint/tests/test_crypt_utils.py
@@ -1,5 +1,5 @@
 import pytest
-from crypt_utils import get_salt, sign_string
+from crypt_utils import sign_string
 
 
 @pytest.fixture
@@ -17,18 +17,11 @@ def sample_data() -> str:
     return 'sample data'
 
 
-def test_get_salt() -> None:
-    salt_1 = get_salt()
-    salt_2 = get_salt()
-    assert len(salt_1) == 88
-    assert salt_1 != salt_2
-
-
-def test_get_salt_small() -> None:
-    salt_1 = get_salt(1)
-    assert len(salt_1) == 4
-
-
 def test_sign_string(sample_salt: str, sample_key: str, sample_data: str) -> None:
     actual = sign_string(salt=sample_salt, key=sample_key, data=sample_data)
     assert actual == '8a2dbe236ead75dd242d3ee015b3d8b6'
+
+
+def test_sign_string_no_salt(sample_key: str, sample_data: str) -> None:
+    actual = sign_string(salt=None, key=sample_key, data=sample_data)
+    assert actual == '188119f4319b02e76b50d297acc5b9cd'
