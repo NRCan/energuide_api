505f02f24bc6c325f88ba2e9ed495091fef7e48f
Steve Astels <sastels@gmail.com>
2018-02-20 08:58:33 -0500

    clean up test code

--- extract_endpoint/tests/test_azure_utils.py
+++ extract_endpoint/tests/test_azure_utils.py
@@ -1,38 +1,36 @@
-import os
 from io import BytesIO
-import typing
+from typing import Optional
 import pytest
 from azure.storage.blob import BlockBlobService
-from azure_utils import StorageCoordinates, upload_stream_to_azure, EnvVariables, DefaultVariables
+from azure_utils import StorageCoordinates, upload_stream_to_azure
 
 
 @pytest.fixture
 def sample_account() -> str:
-    return os.environ.get(EnvVariables.account.value, DefaultVariables.account.value)
+    return 'devstoreaccount1'
 
 
 @pytest.fixture
 def sample_key() -> str:
-    return os.environ.get(EnvVariables.key.value, DefaultVariables.key.value)
+    return 'Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=='
 
 
 @pytest.fixture
 def sample_container() -> str:
-    return os.environ.get(EnvVariables.container.value, DefaultVariables.container.value)
+    return 'endpoint'
 
 
 @pytest.fixture
-def sample_domain() -> typing.Optional[str]:
-    return os.environ.get(EnvVariables.domain.value, DefaultVariables.domain.value)
+def sample_domain() -> Optional[str]:
+    return 'http://127.0.0.1:10000/devstoreaccount1'
 
 
 @pytest.fixture
 def sample_storage_coordinates(sample_account: str,
                                sample_key: str,
                                sample_container: str,
-                               sample_domain: typing.Optional[str]) -> StorageCoordinates:
-    return StorageCoordinates(account=sample_account, key=sample_key,
-                              container=sample_container, domain=sample_domain)
+                               sample_domain: Optional[str]) -> StorageCoordinates:
+    return StorageCoordinates(account=sample_account, key=sample_key, container=sample_container, domain=sample_domain)
 
 
 @pytest.fixture
@@ -47,7 +45,7 @@ def sample_stream_content()->str:
 
 
 @pytest.fixture
-def sample_stream(sample_stream_content) -> BytesIO:
+def sample_stream(sample_stream_content: str) -> BytesIO:
     return BytesIO(sample_stream_content.encode())
 
 
@@ -67,4 +65,8 @@ def test_upload_stream_to_azure(sample_storage_coordinates: StorageCoordinates,
     assert sample_filename in [blob.name for blob in sample_block_blob_service.list_blobs(sample_container)]
     actual = sample_block_blob_service.get_blob_to_text(sample_container, sample_filename)
     sample_block_blob_service.delete_blob(sample_container, sample_filename)
-    assert actual == sample_stream_content
+
+    if isinstance(actual, str):
+        assert actual == sample_stream_content
+    else:
+        assert actual.content == sample_stream_content
