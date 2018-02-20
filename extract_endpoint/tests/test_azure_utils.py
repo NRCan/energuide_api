3bd8ab4b12e38752bbea4f38e2582111ba07c1fc
Steve Astels <sastels@gmail.com>
2018-02-20 16:23:48 -0500

    I need a scotch

--- extract_endpoint/tests/test_azure_utils.py
+++ extract_endpoint/tests/test_azure_utils.py
@@ -5,32 +5,12 @@ import azure_utils
 
 
 @pytest.fixture
-def sample_account() -> str:
-    return 'devstoreaccount1'
-
-
-@pytest.fixture
-def sample_key() -> str:
-    return 'Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=='
-
-
-@pytest.fixture
-def sample_container() -> str:
-    return 'energuide-extracted-data'
-
-
-@pytest.fixture
-def sample_domain() -> str:
-    return 'http://127.0.0.1:10000/devstoreaccount1'
-
-
-@pytest.fixture
-def sample_storage_coordinates(sample_account: str,
-                               sample_key: str,
-                               sample_container: str,
-                               sample_domain: str) -> azure_utils.StorageCoordinates:
-    return azure_utils.StorageCoordinates(account=sample_account, key=sample_key,
-                                          container=sample_container, domain=sample_domain)
+def sample_storage_coordinates() -> azure_utils.StorageCoordinates:
+    return azure_utils.StorageCoordinates(account='devstoreaccount1',
+                                          key='Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uS'
+                                          + 'RZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==',
+                                          container='energuide-extracted-data',
+                                          domain='http://127.0.0.1:10000/devstoreaccount1')
 
 
 @pytest.fixture
@@ -38,10 +18,12 @@ def sample_block_blob_service(sample_storage_coordinates: azure_utils.StorageCoo
     block_blob_service = blob.BlockBlobService(account_name=sample_storage_coordinates.account,
                                                account_key=sample_storage_coordinates.key,
                                                custom_domain=sample_storage_coordinates.domain)
+    yield block_blob_service
+
     container = sample_storage_coordinates.container
     for filename in [blob.name for blob in block_blob_service.list_blobs(container)]:
         block_blob_service.delete_blob(container, filename)
-    return block_blob_service
+
 
 @pytest.fixture
 def sample_stream_content() -> str:
