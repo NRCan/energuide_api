8ea0dc84f90c2d810a3ced305474a5263fca7837
Steve Astels <sastels@gmail.com>
2018-02-20 13:36:03 -0500

    added logging

--- extract_endpoint/src/extract_endpoint.py
+++ extract_endpoint/src/extract_endpoint.py
@@ -1,5 +1,8 @@
 import os
 import base64
+import logging
+from logging import handlers
+
 from flask import Flask, request, abort
 from werkzeug.utils import secure_filename
 from azure_utils import StorageCoordinates, upload_stream_to_azure, EnvVariables, DefaultVariables
@@ -9,7 +12,6 @@ from crypt_utils import sign_string
 DEFAULT_ENDPOINT_SECRET_KEY = 'no key'
 
 App = Flask(__name__)
-App.debug = False
 App.config.update(dict(
     SECRET_KEY=os.environ.get('ENDPOINT_SECRET_KEY', DEFAULT_ENDPOINT_SECRET_KEY),
 ))
@@ -25,34 +27,26 @@ def frontend() -> str:
 @App.route('/upload_file', methods=['POST'])
 def upload_file() -> str:
     if App.config['SECRET_KEY'] == DEFAULT_ENDPOINT_SECRET_KEY:
+        App.logger.error("Need to define environment variable ENDPOINT_SECRET_KEY")
         raise ValueError("Need to define environment variable ENDPOINT_SECRET_KEY")
 
-    if App.debug:
-        if 'signature' not in request.form:
-            return "Error: no signature sent"
-        elif 'salt' not in request.form:
-            return "Error: no salt sent"
-    else:
-        if 'signature' not in request.form or 'salt' not in request.form:
-            abort(404)
+    if 'signature' not in request.form:
+        App.logger.error("No signature sent")
+        abort(404)
+    elif 'salt' not in request.form:
+        App.logger.error("No salt sent")
+        abort(404)
 
-    # check if the post request has a file and filename
     if 'file' not in request.files or getattr(request.files['file'], 'filename', '') == '':
-        if App.debug:
-            return "Error: bad file"
-        else:
-            abort(404)
+        App.logger.error("Bad file")
+        abort(404)
 
     file = request.files['file']
-
-    # check that the signature is valid
     file_as_string = base64.b64encode(file.read()).decode('utf-8')
     signed_file = sign_string(salt=request.form['salt'], key=App.config['SECRET_KEY'], data=file_as_string)
     if request.form['signature'] != signed_file:
-        if App.debug:
-            return f"Sig sent: {request.form['signature']} != {signed_file}"
-        else:
-            abort(404)
+        App.logger.error(f"Sig sent: {request.form['signature']} != {signed_file}")
+        abort(404)
 
     file.seek(0)
     filename = secure_filename(file.filename)
@@ -61,8 +55,19 @@ def upload_file() -> str:
     container = os.environ.get(EnvVariables.container.value, DefaultVariables.container.value)
     domain = os.environ.get(EnvVariables.domain.value, DefaultVariables.domain.value)
     azure_sc = StorageCoordinates(account=account, key=key, container=container, domain=domain)
-    return upload_stream_to_azure(azure_sc, file, filename)
+    if upload_stream_to_azure(azure_sc, file, filename):
+        App.logger.info(f"File {filename} uploaded to Azure")
+        return 'success'
+    App.logger.error(f"File {filename} failed upload to Azure")
+    return 'failure'
 
 
 if __name__ == "__main__":
+    Formatter = logging.Formatter(
+        "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+    Handler = handlers.RotatingFileHandler('extract_endpoint.log', maxBytes=10000000, backupCount=5)
+    Handler.setLevel(logging.DEBUG)
+    App.logger.setLevel(logging.DEBUG)
+    Handler.setFormatter(Formatter)
+    App.logger.addHandler(Handler)
     App.run()
