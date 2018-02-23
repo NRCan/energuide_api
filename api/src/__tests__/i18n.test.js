15bc6681252a475727f6239844c83c7375958268
Paul Craig <paul_craig_16@hotmail.com>
2018-02-23 12:14:04 -0500

    Test that our scalar descriptions match GraphQL's
    We are overriding the descriptions for the default GraphQL scalar
    types, which means that if they ever change we might be out of
    sync.
    
    These tests make sure that our descriptions match those in
    the base types.

---
+++ api/src/__tests__/i18n.test.js
@@ -0,0 +1,104 @@
+import request from 'supertest'
+import Server from '../server'
+import {
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLString,
+  GraphQLBoolean,
+} from 'graphql'
+
+let mockServer = new Server({
+  client: jest.fn(),
+})
+
+const makeRequest = function({ typeName, server = mockServer, lang = 'en' }) {
+  let req = request(server)
+    .post('/graphql')
+    .set('Content-Type', 'application/json; charset=utf-8')
+
+  if (lang) {
+    req.set('Accept-Language', lang)
+  }
+
+  return req.send({
+    query: `query {
+       __type(name: "${typeName}") {
+         name
+         description
+       }
+    }`,
+  })
+}
+
+const replaceBackTicksWithSingleQuotes = function(str) {
+  return str.replace(/`/g, "'").trim()
+}
+
+describe('configuration', () => {
+  describe('graphql default scalar value', () => {
+    it('GraphQLInt returns the same description as I18NInt', async () => {
+      let response = await makeRequest({ typeName: 'I18NInt' })
+
+      let { __type: { description } } = response.body.data
+      expect(description).toEqual(
+        replaceBackTicksWithSingleQuotes(GraphQLInt.description),
+      )
+    })
+
+    it('GraphQLFloat returns the same description as I18NFloat', async () => {
+      let response = await makeRequest({ typeName: 'I18NFloat' })
+
+      let { __type: { description } } = response.body.data
+      expect(description).toEqual(
+        replaceBackTicksWithSingleQuotes(GraphQLFloat.description),
+      )
+    })
+
+    it('GraphQLString returns the same description as I18NString', async () => {
+      let response = await makeRequest({ typeName: 'I18NString' })
+
+      let { __type: { description } } = response.body.data
+      expect(description).toEqual(
+        replaceBackTicksWithSingleQuotes(GraphQLString.description),
+      )
+    })
+
+    it('GraphQLBoolean returns the same description as I18NBoolean', async () => {
+      let response = await makeRequest({ typeName: 'I18NBoolean' })
+
+      let { __type: { description } } = response.body.data
+      expect(description).toEqual(
+        replaceBackTicksWithSingleQuotes(GraphQLBoolean.description),
+      )
+    })
+  })
+
+  describe('i18n', () => {
+    it('returns french description when french language header sent', async () => {
+      let response = await makeRequest({ typeName: 'Evaluation', lang: 'fr' })
+
+      let { __type: { description } } = response.body.data
+      expect(description).toEqual(
+        "Informations détaillées sur les caractéristiques spécifiques d'un logement donné",
+      )
+    })
+
+    it('returns english description when english language header sent', async () => {
+      let response = await makeRequest({ typeName: 'Evaluation', lang: 'en' })
+
+      let { __type: { description } } = response.body.data
+      expect(description).toEqual(
+        'Detailed information about specific features of a given dwelling',
+      )
+    })
+
+    it('defaults to english if no language header is set', async () => {
+      let response = await makeRequest({ typeName: 'Evaluation', lang: '' })
+
+      let { __type: { description } } = response.body.data
+      expect(description).toEqual(
+        'Detailed information about specific features of a given dwelling',
+      )
+    })
+  })
+})
