4eb894ff2a4af830be0e04787a11d30291ec31e5
David Buckley <buckley.w.david@gmail.com>
2018-02-23 15:26:18 -0500

    change to actual nrcan channel, remove failing job

--- scripts/notify_failure.sh
+++ scripts/notify_failure.sh
@@ -1,3 +1,3 @@
 if [ "${CIRCLE_BRANCH}" == "slack_notification" ]; then
-    curl -X POST --data-urlencode "payload={\"channel\": \"#circleci_test\", \"text\": \"<!here> Master is failing\"}" $NRCAN_SLACK_URL
+    curl -X POST --data-urlencode "payload={\"text\": \"<!here> Master is failing!!!\"}" $NRCAN_SLACK_URL
 fi
