a15ed996ffc472ae5199482f1f694439ac343b82
Steve Astels <sastels@users.noreply.github.com>
2018-02-26 13:43:40 -0500

    Extract endpoint (#211)
    Added endpoint to live between NRCAN and Azure storage.

--- extract_endpoint/tests/test_azure_utils.py
+++ extract_endpoint/tests/test_azure_utils.py
@@ -1,17 +1,6 @@
-import io
 import pytest
 from azure.storage import blob
 from extract_endpoint import azure_utils
-
-
-@pytest.fixture
-def azure_service() -> blob.BlockBlobService:
-    azure_service = blob.BlockBlobService(account_name=azure_utils.AZURE_EMULATOR_COORDS.account,
-                                          account_key=azure_utils.AZURE_EMULATOR_COORDS.key,
-                                          custom_domain=azure_utils.AZURE_EMULATOR_COORDS.domain)
-    azure_service.create_container(azure_utils.AZURE_EMULATOR_COORDS.container)
-    yield azure_service
-    azure_service.delete_container(azure_utils.AZURE_EMULATOR_COORDS.container)
 
 
 @pytest.fixture
@@ -20,8 +9,8 @@ def sample_stream_content() -> str:
 
 
 @pytest.fixture
-def sample_stream(sample_stream_content: str) -> io.BytesIO:
-    return io.BytesIO(sample_stream_content.encode())
+def sample_data(sample_stream_content: str) -> bytes:
+    return sample_stream_content.encode()
 
 
 @pytest.fixture
@@ -29,13 +18,14 @@ def sample_filename() -> str:
     return "sample_filename.txt"
 
 
-def test_upload_stream_to_azure(azure_service: blob.BlockBlobService,
-                                sample_stream: io.BytesIO,
-                                sample_stream_content: str,
-                                sample_filename: str) -> None:
+def test_upload_bytes_to_azure(azure_emulator_coords: azure_utils.StorageCoordinates,
+                               azure_service: blob.BlockBlobService,
+                               sample_data: bytes,
+                               sample_stream_content: str,
+                               sample_filename: str) -> None:
 
-    assert azure_utils.upload_stream_to_azure(azure_utils.AZURE_EMULATOR_COORDS, sample_stream, sample_filename)
+    assert azure_utils.upload_bytes_to_azure(azure_emulator_coords, sample_data, sample_filename)
     assert sample_filename in \
-           [blob.name for blob in azure_service.list_blobs(azure_utils.AZURE_EMULATOR_COORDS.container)]
-    actual_file_blob = azure_service.get_blob_to_text(azure_utils.AZURE_EMULATOR_COORDS.container, sample_filename)
+           [blob.name for blob in azure_service.list_blobs(azure_emulator_coords.container)]
+    actual_file_blob = azure_service.get_blob_to_text(azure_emulator_coords.container, sample_filename)
     assert actual_file_blob.content == sample_stream_content
