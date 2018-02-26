a8373ec969dfb9f6864a38a27e24619057dd394d
Steve Astels <sastels@gmail.com>
2018-02-26 14:39:40 -0500

    done door

--- etl/src/energuide/embedded/door.py
+++ etl/src/energuide/embedded/door.py
@@ -4,7 +4,7 @@ from energuide import element
 from energuide.embedded import distance
 from energuide.embedded import area
 from energuide.embedded import insulation
-from energuide.exceptions import InvalidEmbeddedDataTypeError
+from energuide.exceptions import InvalidEmbeddedDataTypeError, ElementGetValueError
 
 
 class _Door(typing.NamedTuple):
@@ -27,11 +27,11 @@ class Door(_Door):
                     english=door.get_text('Construction/Type/English'),
                     french=door.get_text('Construction/Type/French'),
                 ),
-                door_insulation=insulation.Insulation(float(door.xpath('Construction/Type/@value')[0])),
-                height=distance.Distance(float(door.xpath('Measurements/@height')[0])),
-                width=distance.Distance(float(door.xpath('Measurements/@width')[0])),
+                door_insulation=insulation.Insulation(door.get('Construction/Type/@value', float)),
+                height=distance.Distance(door.get('Measurements/@height', float)),
+                width=distance.Distance(door.get('Measurements/@width', float)),
             )
-        except (IndexError, ValueError, AssertionError) as exc:
+        except (ElementGetValueError, AssertionError) as exc:
             raise InvalidEmbeddedDataTypeError(Door) from exc
 
     @property
