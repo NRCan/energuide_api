eed57da5404bbb479e3820d531ef9011329b35b2
evadb <evademersbrett@gmail.com>
2018-02-27 16:15:03 -0500

    Add timestamp

--- extract_endpoint/src/extract_endpoint/post_to_endpoint.py
+++ extract_endpoint/src/extract_endpoint/post_to_endpoint.py
@@ -2,9 +2,9 @@ import os
 import base64
 import secrets
 import typing
+import datetime
 import requests
 import click
-import datetime
 from extract_endpoint import crypt_utils
 
 
@@ -16,7 +16,7 @@ def main() -> None:
     pass
 
 
-def post_stream(stream: typing.IO[bytes], filename: typing.Optional[str], url: str) -> requests.models.Response:
+def post_stream(stream: typing.IO[bytes], filename: typing.Optional[str], url: str, timestamp: str) -> requests.models.Response:
     if filename is None and stream.name == '<stdin>':
         raise ValueError("Must supply a filename if reading from stdin")
     if filename is None:
@@ -27,7 +27,6 @@ def post_stream(stream: typing.IO[bytes], filename: typing.Optional[str], url: s
     signature = crypt_utils.sign_string(salt=salt,
                                         key=os.environ.get('ENDPOINT_SECRET_KEY', DEFAULT_ENDPOINT_SECRET_KEY),
                                         data=base64.b64encode(data).decode('utf-8'))
-    timestamp = datetime.datetime.now()
 
     return requests.post(url=url,
                          files={'file': data},
@@ -38,5 +37,6 @@ def post_stream(stream: typing.IO[bytes], filename: typing.Optional[str], url: s
 @click.argument('stream', type=click.File('rb'))
 @click.option('--filename')
 @click.option('--url', default='http://127.0.0.1:5000/upload_file')
-def upload(stream: typing.IO[bytes], filename: typing.Optional[str], url: str) -> None:
-    click.echo(str(post_stream(stream=stream, filename=filename, url=url)))
+@click.option('--timestamp', default=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+def upload(stream: typing.IO[bytes], filename: typing.Optional[str], url: str, timestamp: str) -> None:
+    click.echo(str(post_stream(stream=stream, filename=filename, url=url, timestamp=timestamp)))
