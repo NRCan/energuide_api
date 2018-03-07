a5eeb3875557e3718bb4721263397867413d6adf
Jason White <jason.white@cds-snc.ca>
2018-03-07 11:50:14 -0500

    add progress bar to transform, clean up output (#329)
    * add progress bar to transform, clean up output
    
    * add new tests for reader#num_rows

--- etl/src/energuide/transform.py
+++ etl/src/energuide/transform.py
@@ -3,6 +3,7 @@ import json
 import os
 import typing
 import zipfile
+from tqdm import tqdm
 import typing_extensions
 from azure.storage import blob
 from energuide import dwelling
@@ -36,6 +37,9 @@ class ExtractProtocol(typing_extensions.Protocol):
     def extracted_rows(self) -> typing.Iterator[typing.Dict[str, typing.Any]]:
         pass
 
+    def num_rows(self) -> int:
+        pass
+
 
 class LocalExtractReader:
 
@@ -47,27 +51,43 @@ class LocalExtractReader:
             for file in zip_input.namelist():
                 content = zip_input.read(file)
                 house = json.loads(content)
-                house['jsonFileName'] = content
+                house['jsonFileName'] = file
                 yield house
+
+    def num_rows(self) -> int:
+        with zipfile.ZipFile(self._zip_filename) as zip_input:
+            return len(zip_input.namelist())
 
 
 class AzureExtractReader:
 
     def __init__(self, coords: AzureCoordinates) -> None:
         self._coords = coords
+        self._azure: typing.Optional[blob.BlockBlobService] = None
+
+    @property
+    def _azure_service(self) -> blob.BlockBlobService:
+        if self._azure is None:
+            self._azure = blob.BlockBlobService(account_name=self._coords.account,
+                                                account_key=self._coords.key,
+                                                custom_domain=self._coords.domain)
+        return self._azure
+
+    def _files(self) -> typing.List[str]:
+        # itertools.groupby() needs its input sorted by the groupby key. We are assuming that that key is the filename
+        files = sorted([blob_.name for blob_ in self._azure_service.list_blobs(self._coords.container)
+                        if 'timestamp' not in blob_.name])
+        return files
 
     def extracted_rows(self) -> typing.Iterator[typing.Dict[str, typing.Any]]:
-        azure_service = blob.BlockBlobService(account_name=self._coords.account,
-                                              account_key=self._coords.key,
-                                              custom_domain=self._coords.domain)
-        # itertools.groupby() needs its input sorted by the groupby key. We are assuming that that key is the filename
-        files = sorted([blob_.name for blob_ in azure_service.list_blobs(self._coords.container)
-                        if 'timestamp' not in blob_.name])
-        for file in files:
-            content = azure_service.get_blob_to_bytes(self._coords.container, file).content
+        for file in self._files():
+            content = self._azure_service.get_blob_to_bytes(self._coords.container, file).content
             house = json.loads(content)
-            house['jsonFileName'] = content
+            house['jsonFileName'] = file
             yield house
+
+    def num_rows(self) -> int:
+        return len(self._files())
 
 
 def _read_groups(extracted_rows: typing.Iterable[typing.Dict[str, typing.Any]]
@@ -89,8 +109,10 @@ def _generate_dwellings(grouped: typing.List[typing.Dict[str, typing.Any]]) -> t
     return None
 
 
-def transform(extract_reader: ExtractProtocol) -> typing.Iterator[dwelling.Dwelling]:
-    for row_group in _read_groups(extract_reader.extracted_rows()):
+def transform(extract_reader: ExtractProtocol, show_progress: bool = False) -> typing.Iterator[dwelling.Dwelling]:
+    extracted_rows = tqdm(extract_reader.extracted_rows(), total=extract_reader.num_rows(),
+                          unit=' files', disable=not show_progress)
+    for row_group in _read_groups(extracted_rows):
         output = _generate_dwellings(row_group)
         if output:
             yield output
