fe446599eb5b0f526e435b063760cc0e7b85d23b
David Buckley <buckley.w.david@gmail.com>
2018-03-12 12:38:42 -0400

    refactor test

--- etl/tests/test_logger.py
+++ etl/tests/test_logger.py
@@ -4,8 +4,8 @@ from energuide import logger
 def test_unwrap_exception_message() -> None:
     try:
         try:
-            1/0
+            raise ZeroDivisionError('ZeroDivisionError Message')
         except ZeroDivisionError as exc:
             raise ValueError('ValueError Message') from exc
     except ValueError as exc:
-        assert logger.unwrap_exception_message(exc, ' - ') == 'ValueError Message - division by zero'
+        assert logger.unwrap_exception_message(exc, ' - ') == 'ValueError Message - ZeroDivisionError Message'
