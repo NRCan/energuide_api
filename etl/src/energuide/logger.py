9419e190ede614cb03b67db19822135630267836
David Buckley <buckley.w.david@gmail.com>
2018-03-12 12:29:32 -0400

    improve TL logging messages

--- etl/src/energuide/logger.py
+++ etl/src/energuide/logger.py
@@ -1,8 +1,19 @@
 import logging
+import typing
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(name)s: %(message)s',
                     datefmt='%Y-%m-%dT%H:%M:%S%z')
+
+
+def unwrap_exception_message(exc: typing.Type[Exception], join: str = ' - ') -> str:
+    if exc.__context__:
+        if exc.args:
+            return f'{exc}{join}{unwrap_exception_message(exc.__context__)}'
+        else:
+            return f'{unwrap_exception_message(exc.__context__)}'
+    else:
+        return f'{exc}'
 
 
 def get_logger(name: str) -> logging.Logger:
