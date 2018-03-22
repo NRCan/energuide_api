c70ff8bf024cfa15fac054ce129157e4c605a51e
David Buckley <buckley.w.david@gmail.com>
2018-03-22 13:47:36 -0400

    add dwelling

---
+++ nrcan_etl/tests/conftest.py
@@ -0,0 +1,20 @@
+import os
+import typing
+import zipfile
+import py
+import pytest
+from energuide import extractor
+
+
+@pytest.fixture
+def energuide_fixture() -> str:
+    return os.path.join(os.path.dirname(__file__), 'scrubbed_random_sample_xml.csv')
+
+
+@pytest.fixture
+def energuide_zip_fixture(tmpdir: py._path.local.LocalPath, energuide_fixture: str) -> str:
+    outfile = os.path.join(tmpdir, 'scrubbed_random_sample_xml.zip')
+
+    data = extractor.extract_data(energuide_fixture)
+    extractor.write_data(data, outfile)
+    return outfile
