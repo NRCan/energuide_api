7f1bd9ac40ceb336675d60d736a4685ecc7fd84b
David Buckley <buckley.w.david@gmail.com>
2018-03-23 10:55:17 -0400

    add serialization of measurment data if applicable

--- nrcan_etl/src/energuide/embedded/measurement.py
+++ nrcan_etl/src/energuide/embedded/measurement.py
@@ -8,7 +8,17 @@ class _Measurement(typing.NamedTuple):
 class Measurement(_Measurement):
 
     def to_dict(self) -> typing.Dict[str, T]:
+        if hasattr(self.measurement, 'to_dict'):
+            measurement = self.measurement.to_dict()
+        else:
+            measurement = self.measurement
+
+        if hasattr(self.upgrade, 'to_dict'):
+            upgrade = self.upgrade.to_dict()
+        else:
+            upgrade = self.upgrade
+
         return {
-            'measurement': self.measurement,
-            'upgrade': self.upgrade
+            'measurement': measurement,
+            'upgrade': measurement,
         }
