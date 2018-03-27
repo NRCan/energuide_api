d00dfa2686e5957d9b15a7513f86e714be5d9cb6
David Buckley <buckley.w.david@gmail.com>
2018-03-27 10:16:59 -0400

    Add F evaluation type (#426)
    add F evaluation type

--- nrcan_etl/src/energuide/embedded/evaluation_type.py
+++ nrcan_etl/src/energuide/embedded/evaluation_type.py
@@ -1,10 +1,12 @@
 import enum
 from energuide.exceptions import InvalidInputDataError
 
+
 @enum.unique
 class EvaluationType(enum.Enum):
     PRE_RETROFIT = 'D'
     POST_RETROFIT = 'E'
+    INCENTIVE_PROGRAM = 'F'
 
     @classmethod
     def from_code(cls, code: str) -> 'EvaluationType':
@@ -12,5 +14,6 @@ class EvaluationType(enum.Enum):
             return EvaluationType.PRE_RETROFIT
         elif code == cls.POST_RETROFIT.value:
             return EvaluationType.POST_RETROFIT
-        else:
-            raise InvalidInputDataError(f'Invalid code: {code}')
+        elif code == cls.INCENTIVE_PROGRAM:
+            return EvaluationType.INCENTIVE_PROGRAM
+        raise InvalidInputDataError(f'Invalid code: {code}')
