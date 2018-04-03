2666a2855e095120c876279b14ae6e821ba78bca
David Buckley <buckley.w.david@gmail.com>
2018-04-03 16:38:25 -0400

    make region parsing much more robust

--- nrcan_etl/src/energuide/embedded/region.py
+++ nrcan_etl/src/energuide/embedded/region.py
@@ -10,6 +10,34 @@ from energuide import logger
 LOGGER = logger.get_logger(__name__)
 
 
+_FRENCH_NAMES = {
+    'ALBERTA': 'ALBERTA',
+    'COLOMBIE_BRITANNIQUE': 'BRITISH_COLUMBIA',
+    'ILE_DU_PRINCE_EDOUARD': 'PRINCE_EDWARD_ISLAND',
+    'MANITOBA': 'MANITOBA',
+    'NOUVEAU_BRUNSWICK': 'NEW_BRUNSWICK',
+    'NOUVELLE_ECOSSE': 'NOVA_SCOTIA',
+    'NUNAVUT': 'NUNAVUT',
+    'ONTARIO': 'ONTARIO',
+    'QUEBEC': 'QUEBEC',
+    'SASKATCHEWAN': 'SASKATCHEWAN',
+    'TERRE_NEUVE_ET_LABRADOR': 'NEWFOUNDLAND_AND_LABRADOR',
+    'TERRITOIRES_DU_NORD_OUEST': 'NORTHWEST_TERRITORIES',
+    'YUKON': 'YUKON'
+}
+
+_ALTERNATIVE_NAMES = {
+    'YUKON_TERRITORY': 'YUKON',
+    'NORTHWEST_TERRITORY': 'NORTHWEST_TERRITORIES',
+}
+
+_ALTERNATIVE_CODES = {
+    'PEI': 'PE',
+    'NWT': 'NT'
+}
+
+_MINIMUM_SIMILARITY = 0.6
+
 @enum.unique
 class Region(enum.Enum):
     BRITISH_COLUMBIA = 'BC'
@@ -27,34 +55,17 @@ class Region(enum.Enum):
     NUNAVUT = 'NU'
     UNKNOWN = '??'
 
-    _FRENCH_NAMES = {
-        'ALBERTA': 'ALBERTA',
-        'COLOMBIE_BRITANNIQUE': 'BRITISH_COLUMBIA',
-        'ILE_DU_PRINCE_EDOUARD': 'PRINCE_EDWARD_ISLAND',
-        'MANITOBA': 'MANITOBA',
-        'NOUVEAU_BRUNSWICK': 'NEW_BRUNSWICK',
-        'NOUVELLE_ECOSSE': 'NOVA_SCOTIA',
-        'NUNAVUT': 'NUNAVUT',
-        'ONTARIO': 'ONTARIO',
-        'QUEBEC': 'QUEBEC',
-        'SASKATCHEWAN': 'SASKATCHEWAN',
-        'TERRE_NEUVE_ET_LABRADOR': 'NEWFOUNDLAND_AND_LABRADOR',
-        'TERRITOIRES_DU_NORD_OUEST': 'NORTHWEST_TERRITORIES',
-        'YUKON': 'YUKON'
-    }
-
-    _ALTERNATIVE_NAMES = {
-        'YUKON_TERRITORY': 'YUKON'
-    }
-
     @classmethod
     def _from_name(cls, name: str) -> typing.Optional['Region']:
         english_region = Region[name] if name in Region.__members__ else None
-        if english_name:
+        if english_region:
             return english_region
 
-        if name in cls._FRENCH_NAMES:
-            return Region[cls._FRENCH_NAMES[name]]
+        if name in _FRENCH_NAMES:
+            return Region[_FRENCH_NAMES[name]]
+
+        if name in _ALTERNATIVE_NAMES:
+            return Region[_ALTERNATIVE_NAMES[name]]
 
         return None
 
@@ -63,7 +74,7 @@ class Region(enum.Enum):
     def _from_code(cls, code: str) -> typing.Optional['Region']:
         code = code.replace('.', '')
         for region in Region:
-            if code == region.value:
+            if code == region.value or _ALTERNATIVE_CODES.get(code) == region.value:
                 return region
 
         return None
@@ -71,32 +82,42 @@ class Region(enum.Enum):
 
     @classmethod
     def _from_fuzzy_match(cls, data: str) -> typing.Optional['Region']:
-        # Prefix
-        for region in Region:
-            if region.value.startswith(data):
+        # Name is prefix of full name
+        for region in Region.__members__:
+            if region.startswith(data):
                 LOGGER.warning(f'PREFIX: Interpreting {data} as {region}')
-                return region
+                return Region[region]
 
+        # Code separated by _
+        code = data.replace('_', '')
+        region = cls._from_code(code)
+        if region:
+            return region
+
+        # Levenshtein minimum number of edits to closest region
         ratios = [(Levenshtein.ratio(region, data), region) for region in Region.__members__.keys() - {'UNKNOWN'}]
         ratio, region = max(ratios)
 
-        if ratio > 0.2:
-            LOGGER.warning(f'Interpreting {data} as {region}')
+        if ratio > _MINIMUM_SIMILARITY:
+            LOGGER.warning(f'SIMILARITY: Interpreting {data} as {region}')
             return Region[region]
+
+        return None
 
     @classmethod
     def from_data(cls, data: str) -> 'Region':
-        if not data:
+        if len(data) < 2:
             return Region.UNKNOWN
 
         snake_name = re.sub('[ -]', '_', data.upper()).strip('.')
         ascii_name = unicodedata.normalize('NFD', snake_name).encode('ascii', 'ignore').decode()
+        alphabet_name = re.sub('[^a-zA-Z_]', '', ascii_name)
 
-        output = cls._from_name(ascii_name)
+        output = cls._from_name(alphabet_name)
         if not output:
-            output = cls._from_code(ascii_name)
+            output = cls._from_code(alphabet_name)
         if not output:
-            output = cls._from_fuzzy_match(ascii_name)
+            output = cls._from_fuzzy_match(alphabet_name)
         if not output:
             output = Region.UNKNOWN
 
