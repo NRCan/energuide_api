6efa9fc8f649a7c2292ffdf7732dea0f653823aa
David Buckley <buckley.w.david@gmail.com>
2018-04-05 13:28:48 -0400

    Wall exception handling (#464)
    add exception handling to wall datatype

--- nrcan_etl/tests/embedded/test_walls.py
+++ nrcan_etl/tests/embedded/test_walls.py
@@ -1,5 +1,8 @@
+import pytest
+
 from energuide.embedded import walls
 from energuide.embedded import composite
+from energuide.exceptions import InvalidEmbeddedDataTypeError
 
 
 def test_from_data() -> None:
@@ -62,3 +65,21 @@ def test_wall_missing_accepted() -> None:
         'heatLost': None,
     }
 
+
+def test_bad_insulation_string() -> None:
+    insulation = '50;2.3;50;5..7'
+    heat_lost = 10
+
+    with pytest.raises(InvalidEmbeddedDataTypeError) as excinfo:
+        walls.Wall.from_data(insulation, heat_lost)
+
+    assert excinfo.value.data_class == walls.Wall
+
+
+def test_incomplete_insulation_string() -> None:
+    insulation = '50;2.3;50'
+    heat_lost = 10
+
+    wall = walls.Wall.from_data(insulation, heat_lost)
+
+    wall.insulation[1].value == 0.0
