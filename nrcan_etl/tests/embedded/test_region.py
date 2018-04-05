e7737c15f164af6ae35573f166bc66fd19abb4c9
David Buckley <buckley.w.david@gmail.com>
2018-04-05 12:54:06 -0400

    Even moar testing (#438)
    add more comprehensive enum tests

--- nrcan_etl/tests/embedded/test_region.py
+++ nrcan_etl/tests/embedded/test_region.py
@@ -8,6 +8,16 @@ class TestRegion:
             'Ontario',
             'british columbia',
             'NOVA SCOTIA',
+            'ALBERTA',
+            'SASKATCHEWAN',
+            'MANITOBA',
+            'QUEBEC',
+            'NEW_BRUNSWICK',
+            'PRINCE_EDWARD_ISLAND',
+            'NEWFOUNDLAND_AND_LABRADOR',
+            'YUKON',
+            'NORTHWEST_TERRITORIES',
+            'NUNAVUT',
         ]
         output = [region.Region.from_data(row) for row in data]
 
@@ -15,6 +25,16 @@ class TestRegion:
             region.Region.ONTARIO,
             region.Region.BRITISH_COLUMBIA,
             region.Region.NOVA_SCOTIA,
+            region.Region.ALBERTA,
+            region.Region.SASKATCHEWAN,
+            region.Region.MANITOBA,
+            region.Region.QUEBEC,
+            region.Region.NEW_BRUNSWICK,
+            region.Region.PRINCE_EDWARD_ISLAND,
+            region.Region.NEWFOUNDLAND_AND_LABRADOR,
+            region.Region.YUKON,
+            region.Region.NORTHWEST_TERRITORIES,
+            region.Region.NUNAVUT,
         ]
 
     def test_from_unknown_name(self):
@@ -22,15 +42,36 @@ class TestRegion:
 
     def test_from_code(self):
         data = [
-            'ON',
-            'bc',
-            'Ns',
+            'bC',
+            'AB',
+            'sk',
+            'MB',
+            'on',
+            'Qc',
+            'NB',
+            'pE',
+            'NS',
+            'NL',
+            'Yt',
+            'NT',
+            'nU',
         ]
+
         output = [region.Region.from_data(row) for row in data]
         assert output == [
-            region.Region.ONTARIO,
             region.Region.BRITISH_COLUMBIA,
+            region.Region.ALBERTA,
+            region.Region.SASKATCHEWAN,
+            region.Region.MANITOBA,
+            region.Region.ONTARIO,
+            region.Region.QUEBEC,
+            region.Region.NEW_BRUNSWICK,
+            region.Region.PRINCE_EDWARD_ISLAND,
             region.Region.NOVA_SCOTIA,
+            region.Region.NEWFOUNDLAND_AND_LABRADOR,
+            region.Region.YUKON,
+            region.Region.NORTHWEST_TERRITORIES,
+            region.Region.NUNAVUT,
         ]
 
     def test_from_unknown_code(self):
